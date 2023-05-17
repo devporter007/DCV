2bd142d93c811cdb96c0ee42ee2db8c3835ec613
devporter007 <portzero7@protonmail.com>
2023-05-17 23:57:23 +0530

    fixed memory issues, bare imp of hdrtoast

---
+++ app/src/main/java/com/port/dcv/HdrPopup.kt
@@ -0,0 +1,11 @@
+package com.port.dcv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HdrPopup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hdr_popup)
+    }
+}
--- app/src/main/java/com/port/dcv/MainActivity.kt
+++ app/src/main/java/com/port/dcv/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.port.dcv
 
-import android.content.pm.ActivityInfo
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -8,51 +8,89 @@ import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
-import androidx.media3.datasource.DefaultDataSource
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.source.ProgressiveMediaSource
-import androidx.media3.ui.PlayerView
-
-
-
+import com.port.dcv.databinding.ActivityMainBinding
+import com.port.dcv.databinding.ActivityHdrPopupBinding
 
 class MainActivity : AppCompatActivity() {
+    private var playWhenReady = true
+    private var mediaItemIndex = 0
+    private var playbackPosition = 0L
+    private var player: ExoPlayer? = null
+    private val viewBinding by lazy(LazyThreadSafetyMode.NONE){
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val hdrView by lazy(LazyThreadSafetyMode.NONE){
+        ActivityHdrPopupBinding.inflate(layoutInflater)
+    }
 
-    lateinit var playerView:PlayerView
-    lateinit var player:ExoPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val windowInsetController = WindowCompat.getInsetsController(window,window.decorView)
+        setContentView(viewBinding.root)
+        viewBinding.playerView.setBackgroundColor(Color.BLACK)
         val layoutParams = window.attributes
         layoutParams.screenBrightness = 1.0f
-        window.attributes = layoutParams
-        windowInsetController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        windowInsetController.hide(WindowInsetsCompat.Type.systemBars())
-        setContentView(R.layout.activity_main)
-        playerView = findViewById(R.id.player_view)
-       // requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        val hdrLabel = hdrView.textView
+
+        hdrLabel.text = ""
+    }
 
 
-        val player = ExoPlayer.Builder(this)
+    override fun onStart(){
+        super.onStart()
+        initializePlayer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (player == null){
+            initializePlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        releasePlayer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+    private val urlOfVideo = "https://www.dropbox.com/s/tofb56970h4vpq6/Zack.Snyders.Justice.League.2021.2160p.HMAX.WEB-DL.DDP5.1.Atmos.HDR.HEVC-EVO.mkv?raw=1"
+    private fun initializePlayer(){
+        player = ExoPlayer.Builder(this)
             .build()
-        playerView.player = player
-        playerView.setBackgroundColor(Color.BLACK)
+            .also{exoPlayer -> viewBinding.playerView.player = exoPlayer
+                val mediaItem = MediaItem.fromUri(urlOfVideo)
+            exoPlayer.setMediaItem(mediaItem)
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.seekTo(mediaItemIndex,playbackPosition)
+                exoPlayer.prepare()
+
+            }
 
 
-        val mediaItem = MediaItem.Builder()
-            .setUri("https://www.dropbox.com/s/tofb56970h4vpq6/Zack.Snyders.Justice.League.2021.2160p.HMAX.WEB-DL.DDP5.1.Atmos.HDR.HEVC-EVO.mkv?raw=1")
-            .build()
-        val mediaSource = ProgressiveMediaSource.Factory(
-            DefaultDataSource.Factory(this)
-        ).createMediaSource(mediaItem)
-
-        player.setMediaItem(mediaItem)
-
-
-
-//        player.prepare()
-        player.play()
+    }
+    private fun releasePlayer() {
+        player?.let { player ->
+            playbackPosition = player.currentPosition
+            mediaItemIndex = player.currentMediaItemIndex
+            playWhenReady = player.playWhenReady
+            player.release()
+        }
+        player = null
+    }
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, viewBinding.playerView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     }
 
 
